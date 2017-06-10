6eb2830a54f1748cf79629f158c7883ee99701a8
Krati Ahuja <kratiahuja@gmail.com>
2017-06-10 10:14:56 -0700

    Update addon for FastBoot 1.0

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 var fs   = require('fs');
 var path = require('path');
 var Funnel = require('broccoli-funnel');
+var Merge = require('broccoli-merge-trees');
+var existsSync = require('exists-sync');
+var fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cli-photoswipe',
@@ -11,13 +14,33 @@ module.exports = {
   included: function(app) {
     this.app  = app;
     var psDir = app.bowerDirectory + '/photoswipe';
+    app.import(psDir + '/dist/photoswipe.css');
+    app.import(psDir + '/dist/default-skin/default-skin.css');
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(psDir + '/dist/photoswipe.css');
-      app.import(psDir + '/dist/default-skin/default-skin.css');
-      app.import(psDir + '/dist/photoswipe.js');
-      app.import(psDir + '/dist/photoswipe-ui-default.min.js');
+    console.log(psDir);
+    app.import('vendor/photoswipe/photoswipe.js');
+    app.import('vendor/photoswipe/photoswipe-ui-default.min.js');
+  },
+
+  treeForVendor: function(tree) {
+    var trees = [];
+
+    if (tree) {
+      trees.push(tree);
     }
+
+    var photoswipePath = path.join(this.app.bowerDirectory, 'photoswipe', 'dist');
+
+    if (existsSync(photoswipePath)) {
+      var photoSwipeTrees = fastbootTransform(new Funnel(photoswipePath, {
+        files: ['photoswipe.js', 'photoswipe-ui-default.min.js'],
+        destDir: 'photoswipe'
+      }));
+
+      trees.push(photoSwipeTrees);
+    }
+
+    return new Merge(trees);
   },
 
   treeForPublic: function() {
